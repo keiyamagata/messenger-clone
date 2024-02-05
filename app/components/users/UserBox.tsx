a2dfc1810f43d4ddf601c049c5fb3a76c46705e0
keiyamagata <kei.yamagata@icloud.com>
2024-02-05 13:24:36 +0100

    feat: add loading state between pages

--- app/components/users/UserBox.tsx
+++ app/components/users/UserBox.tsx
@@ -5,6 +5,7 @@ import axios from "axios";
 import { User } from "@prisma/client";
 import { useRouter } from "next/navigation";
 import Avatar from "../avatar/Avatar";
+import LoadingModal from "../modals/LoadingModal";
 
 interface UserBoxProps {
   data: User;
@@ -24,19 +25,22 @@ const UserBox: React.FC<UserBoxProps> = ({ data }) => {
   }, [data, router]);
 
   return (
-    <div
-      onClick={handleClick}
-      className="relative w-full flex items-center space-x-3 bg-white p-3 hover:bg-neutral-100 rounded-lg transition cursor-pointer"
-    >
-      <Avatar user={data} />
-      <div className="min-w-0 flex-1">
-        <div className="focus:outline-none">
-          <div className="flex justify-between items-center mb-1">
-            <p className="text-sm font-medium text-gray-900">{data.name}</p>
+    <>
+      {isLoading && <LoadingModal />}
+      <div
+        onClick={handleClick}
+        className="relative w-full flex items-center space-x-3 bg-white p-3 hover:bg-neutral-100 rounded-lg transition cursor-pointer"
+      >
+        <Avatar user={data} />
+        <div className="min-w-0 flex-1">
+          <div className="focus:outline-none">
+            <div className="flex justify-between items-center mb-1">
+              <p className="text-sm font-medium text-gray-900">{data.name}</p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
